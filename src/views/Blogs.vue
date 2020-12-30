840e625177dc88ec19e66f6a8c03351ef7fbb0a7
hanyh <yunhaoha@usc.edu>
2020-12-30 18:51:00 +0800

    first commit

---
+++ src/views/Blogs.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="mcontaner">
+    <Header></Header>
+
+    <div class="block">
+      <el-timeline>
+
+        <el-timeline-item :timestamp="blog.created" placement="top" v-for="blog in blogs">
+          <el-card>
+            <h4>
+              <router-link :to="{name: 'BlogDetail', params: {blogId: blog.id}}">
+                {{blog.title}}
+              </router-link>
+            </h4>
+            <p>{{blog.description}}</p>
+
+            <span class="mdelete" >
+
+              <el-link type="primary" icon="el-icon-delete" @click="deleteBlog(blog.id)">删除</el-link>
+
+            </span>
+
+          </el-card>
+        </el-timeline-item>
+
+      </el-timeline>
+
+      <el-pagination class="mpage"
+                     background
+                     layout="prev, pager, next"
+                     :current-page="currentPage"
+                     :page-size="pageSize"
+                     :total="total"
+                     @current-change=page>
+      </el-pagination>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Header from "../components/Header";
+  // import hasLogin from Header.data();
+
+  export default {
+    inject: ['reload'], // 引入方法
+    name: "Blogs.vue",
+    components: {Header},
+
+
+    data() {
+      return {
+        blogs: {},
+        currentPage: 1,
+        total: 0,
+        pageSize: 5
+      }
+    },
+    methods: {
+      page(currentPage) {
+        const _this = this
+        _this.$axios.get("/blogs?currentPage=" + currentPage).then(res => {
+          console.log(res)
+          _this.blogs = res.data.data.records
+          _this.currentPage = res.data.data.current
+          _this.total = res.data.data.total
+          _this.pageSize = res.data.data.size
+
+        })
+      },
+      deleteBlog(id) {
+
+        if(confirm('确定要删除吗')==true){
+          this.$axios.delete('/deleteBlog/' +id).then(
+              window.location.reload()
+          )
+        }
+
+      }
+    },
+    created() {
+      this.page(1)
+    }
+  }
+
+
+</script>
+
+<style scoped>
+
+  .mpage {
+    margin: 0 auto;
+    text-align: center;
+  }
+  .mdelete{
+    position: absolute;
+    left: 829px;
+    width: 303px;
+    top: 125px;
+    padding: 0px;
+  }
+</style>
---
+++ src/views/index.vue
@@ -0,0 +1,13 @@
+<template>
+<p1>这是首页</p1>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
